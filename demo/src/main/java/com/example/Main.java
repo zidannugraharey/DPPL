48916a13cc12313cc7ee2d9c7d932665ff2c38b8
zidannugraharey <zidanrey18@gmail.com>
2025-11-10 10:18:58 +0700

    Buat login page
    Login page nya dengan NIM "Admin" dan password "Admin123"

--- demo/src/main/java/com/example/Main.java
+++ demo/src/main/java/com/example/Main.java
@@ -1,8 +1,14 @@
 package com.example;
 
+// Program ini dibuat oleh kelompok 2
+
 public class Main {
     public static void main(String[] args) {
         //Hallo guys
         System.out.println("Hello world!");
+        
+        LoginPageFrame frame = new LoginPageFrame();
+        frame.setTitle("SISTEM PENILAIAN KINERJA DOSEN");
+        frame.setVisible(true);
     }
 }
